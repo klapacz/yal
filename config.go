70dc14d629d9bed42bebe8ea14a5fc33fcbffa96
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-09-01 01:55:42 +0200

    add 'init' subcommand

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"os/user"
 
 	"github.com/pelletier/go-toml/v2"
 )
@@ -15,20 +16,58 @@ type Config struct {
 	Archive Archive
 }
 
-const configFilePath = ".yal/config.toml"
+const configDir = ".yal/"
+const configFileName = "config.toml"
+const configFilePath = configDir + configFileName
 
-func GetConfig() (Config, error) {
+func GetConfig() (*Config, error) {
 	c := Config{}
 
 	dat, err := os.ReadFile(configFilePath)
 	if err != nil {
-		return c, err
+		return &c, err
 	}
 
 	err = toml.Unmarshal(dat, &c)
 	if err != nil {
-		return c, err
+		return &c, err
 	}
 
-	return c, nil
+	return &c, nil
+}
+
+func CreateConfig() error {
+	var username string
+	// get current user name to use as default owner
+	user, err := user.Current()
+	if err != nil {
+		username = ""
+	} else {
+		username = user.Username
+	}
+
+	// create default config and turn it into toml
+	config := Config{Archive: Archive{Owner: username}}
+	b, err := toml.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	// create config directory if does not exist
+	err = os.MkdirAll(configDir, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	// write default config to file, fail if config file already exists
+	f, err := os.OpenFile(configFilePath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/google/uuid"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "init" {
+		err := CreateConfig()
+		if err != nil {
+			log.Fatalf("Error creating configuration file: %s", err)
+		}
+		return
+	}
+
 	config, err := GetConfig()
 	if err != nil {
 		log.Fatalf("Error reading configuration file: %s", err)
